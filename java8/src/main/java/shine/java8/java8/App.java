f860c1f675577d87271ff07c742f9fb9faafa12d
shinealex <shinekuttiadi@gmail.com>
2019-11-09 23:33:13 +0000

    first commit

---
+++ java8/src/main/java/shine/java8/java8/App.java
@@ -0,0 +1,13 @@
+package shine.java8.java8;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
